a1f6e4be79f5394f056e4eb6664d9cedac9b17b4
Drishti Malavade <102899430+DrishtiMalavade@users.noreply.github.com>
2024-04-04 15:38:52 +0530

    homepage.py

--- homepage.py
+++ homepage.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MiniProject",
+    page_icon= "💻"
+)
+
+st.title("HomePage")
+st.markdown("This is a project which includes a yt summerizer named SummerEaze and a Live transcript recorder which saves transcripts of live lectures.")
+st.sidebar.success("Select a page")
 
